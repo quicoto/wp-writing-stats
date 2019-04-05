40094a76b7eecc844e52793c8194672dceaff6a6
Ricard Torres <ricard.torres@netcentric.biz>
2019-04-05 16:48:01 +0200

    Init the plugin file

---
+++ wp-writing-stats.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Writing Stats
+Plugin URI:
+Description: Shows stats on your writing
+Author: Ricard Torres
+Version: 1.0.0
+Author URI: http://php.quicoto.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
